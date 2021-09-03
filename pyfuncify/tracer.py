5fa1d2c0b005d89165b64d0817d31ca2484223dd
Col Perks <wild.fauve@gmail.com>
2021-09-03 12:40:24 +1200

    update tracer

--- pyfuncify/tracer.py
+++ pyfuncify/tracer.py
@@ -2,15 +2,19 @@ import uuid
 
 class Tracer():
 
-    def __init__(self, env):
+    def __init__(self, env, aws_context=None):
         self.env = env
+        self.aws_context = aws_context
         self.handler_id = uuid.uuid4()
 
     def serialise(self):
-        return {'env': self.env, 'handler_id': self.handler_id_to_s()}
+        return {'env': self.env, 'handler_id': self.handler_id_to_s(), 'aws_request_id': aws_request_id()}
 
     def handler_id_to_s(self):
         return str(self.handler_id) if isinstance(self.handler_id, uuid.UUID) else self.handler_id
+
+    def aws_request_id(self):
+        return self.aws_context.aws_request_id if self.aws_context else None
 
 
 def init_tracing(env):
