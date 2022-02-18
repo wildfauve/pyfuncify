b48e3c3314d47c7dbba78689a28071528ba9ed39
Col Perks <wild.fauve@gmail.com>
2022-02-19 09:06:38 +1300

    bump version

--- pyfuncify/app.py
+++ pyfuncify/app.py
@@ -74,7 +74,7 @@ ApiGatewayRequestEvent = app_value.ApiGatewayRequestEvent
 S3StateChangeEvent = app_value.S3StateChangeEvent
 S3Object = app_value.S3Object
 
-Serialise = app_serialisers.Serialiser
+Serialiser = app_serialisers.Serialiser
 DictToJsonSerialiser = app_serialisers.DictToJsonSerialiser
 
 AppError = app_value.AppError
