a1ccaa9165a4436c125b7855808c31b315e1a4ed
Col Perks <wild.fauve@gmail.com>
2022-03-15 09:42:11 +1300

    add record at-prop

--- pyfuncify/record.py
+++ pyfuncify/record.py
@@ -6,3 +6,8 @@ def at(attr, rec):
     return getattr(rec, attr, None)
 
 
+@curry(2)
+def at_prop(prop, rec):
+    property = getattr(rec, prop, None)
+    return property() if property and callable(property) else None
+
--- tests/test_record.py
+++ tests/test_record.py
@@ -5,8 +5,16 @@ from dataclasses import dataclass
 @dataclass
 class Rec():
     attr1: str
+    def attr2(self):
+        return "there"
 
 
 def test_at():
     assert record.at('attr1')(Rec(attr1="hello")) == "hello"
 
+def test_at_prop():
+    assert record.at_prop('attr2')(Rec(attr1="hello")) == "there"
+
+
+def test_at_prop_when_not_callable():
+    assert not record.at_prop('attr1')(Rec(attr1="hello"))
